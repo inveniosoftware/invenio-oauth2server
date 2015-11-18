95b5c491073ba89430ba6b648f565b1366a6079c
Sami Hiltunen <sami.mikael.hiltunen@cern.ch>
2015-11-18 14:52:54 +0100

    models: test_deletion_of_consumer_resource_owner passes

--- tests/test_models.py
+++ tests/test_models.py
@@ -117,3 +117,93 @@ def test_registering_invalid_scope(app):
     with app.app_context():
         with pytest.raises(RegistryError):
             scopes.register('test:scope')
+
+
+def test_deletion_of_consumer_resource_owner(app):
+    """Test deleting of connected user."""
+    with app.app_context():
+        resource_owner = None
+        consumer = None
+        user1_client1 = None
+        u1c1u1t1 = None
+        u1c1u2t2 = None
+        with db.session.begin_nested():
+            resource_owner = User(email='resource_owner@invenio-software.org',
+                                  password='test')
+            consumer = User(email='consumer@invenio-software.org',
+                            password='test')
+            # create resource_owner -> client_1
+            user1_client1 = Client(client_id='client_test_u1c1',
+                                   client_secret='client_test_u1c1',
+                                   name='client_test_u1c1',
+                                   description='',
+                                   is_confidential=False,
+                                   user=resource_owner,
+                                   _redirect_uris='',
+                                   _default_scopes=""
+                                   )
+            # create resource_owner -> client_1 / resource_owner -> token_1
+            u1c1u1t1 = Token(client=user1_client1,
+                             user=resource_owner,
+                             token_type='u',
+                             access_token='dev_access_1',
+                             refresh_token='dev_refresh_1',
+                             expires=None,
+                             is_personal=False,
+                             is_internal=False,
+                             _scopes='',
+                             )
+            # create consumer -> client_1 / resource_owner -> token_2
+            u1c1u2t2 = Token(client=user1_client1,
+                             user=consumer,
+                             token_type='u',
+                             access_token='dev_access_2',
+                             refresh_token='dev_refresh_2',
+                             expires=None,
+                             is_personal=False,
+                             is_internal=False,
+                             _scopes='',
+                             )
+            db.session.add(resource_owner)
+            db.session.add(consumer)
+            db.session.add(user1_client1)
+            db.session.add(u1c1u1t1)
+            db.session.add(u1c1u2t2)
+        uid_1 = resource_owner.id
+        cid_1 = user1_client1.client_id
+        tid_1 = u1c1u1t1.id
+        tid_2 = u1c1u2t2.id
+
+        # start testing
+
+        # delete consumer
+        with db.session.begin_nested():
+            db.session.delete(consumer)
+
+        # assert that t2 deleted
+            assert db.session.query(
+                Token.query.filter(
+                    Token.id == tid_2).exists()).scalar() is False
+        # still exist resource_owner and client_1 and token_1
+            assert db.session.query(
+                User.query.filter(User.id == uid_1).exists()).scalar() is True
+
+            assert db.session.query(
+                Client.query.filter(
+                    Client.client_id == cid_1).exists()).scalar() is True
+
+            assert db.session.query(
+                Token.query.filter(Token.id == tid_1).exists()).scalar() is \
+                True
+
+        # delete resource_owner
+            db.session.delete(resource_owner)
+
+        # still resource_owner and client_1 and token_1 deleted
+            assert db.session.query(
+                Client.query.filter(
+                    Client.client_id == cid_1).exists()).scalar() is False
+
+            assert db.session.query(
+                Token.query.filter(Token.id == tid_1).exists()).scalar() is \
+                False
