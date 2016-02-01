7718ca68b25082370b409debe309fea3439b2f06
Sami Hiltunen <sami.mikael.hiltunen@cern.ch>
2016-02-01 14:54:09 +0100

    installation: pydocstyle>=1.0.0
    * Replaces pep257 with pydocstyle.
    
    Signed-off-by: Sami Hiltunen <sami.mikael.hiltunen@cern.ch>

--- invenio_oauth2server/decorators.py
+++ invenio_oauth2server/decorators.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015 CERN.
+# Copyright (C) 2015, 2016 CERN.
 #
 # Invenio is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
@@ -46,7 +46,7 @@ def require_api_auth():
 
         @wraps(f)
         def decorated(*args, **kwargs):
-            """OAuth 2.0 Authentication."""
+            """Require OAuth 2.0 Authentication."""
             resp = f_oauth_required(*args, **kwargs)
             return resp
         return decorated
--- invenio_oauth2server/forms.py
+++ invenio_oauth2server/forms.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2014, 2015 CERN.
+# Copyright (C) 2014, 2015, 2016 CERN.
 #
 # Invenio is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
@@ -118,6 +118,8 @@ class ClientFormBase(model_form_factory(Form)):
     """Base class for Client form."""
 
     class Meta:
+        """Metadata class."""
+
         model = Client
         exclude = [
             'client_secret',
