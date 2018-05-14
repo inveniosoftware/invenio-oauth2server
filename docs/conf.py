ea98461de50b06051c2c1eb30cbf819dc52322a6
Diego Rodriguez <diego.rodriguez@cern.ch>
2018-05-14 13:55:17 +0200

    docs: point intersphinx to Flask 0.12
    * (closes #181).

--- docs/conf.py
+++ docs/conf.py
@@ -319,7 +319,7 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'https://docs.python.org/': None,
-    'flask': ('http://flask.pocoo.org/docs/0.11', None),
+    'flask': ('http://flask.pocoo.org/docs/0.12', None),
 }
 
 # Autodoc configuraton.
