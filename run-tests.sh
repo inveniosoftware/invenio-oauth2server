d14da46236d78bebdb7bd2092de0d2979f522d04
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-06-11 23:42:30 +0200

    move check_manifest configuration to setup.cfg.
    concentrate the configuration of all calls in one place

--- run-tests.sh
+++ run-tests.sh
@@ -4,6 +4,7 @@
 # This file is part of Invenio.
 # Copyright (C) 2015-2018 CERN.
 # Copyright (C) 2022 RERO.
+# Copyright (C) 2022 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -23,7 +24,7 @@ function cleanup {
 }
 trap cleanup EXIT
 
-python -m check_manifest --ignore ".*-requirements.txt"
+python -m check_manifest
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 eval "$(docker-services-cli up --db ${DB:-postgresql} --env)"
 python -m pytest
