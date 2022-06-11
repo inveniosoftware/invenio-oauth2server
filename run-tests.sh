97619bfb8801c9fb0113d5212b3f9e80e0914a7a
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-06-11 23:56:01 +0200

    global: clean test infrastructure

--- run-tests.sh
+++ run-tests.sh
@@ -29,5 +29,4 @@ python -m sphinx.cmd.build -qnNW docs docs/_build/html
 eval "$(docker-services-cli up --db ${DB:-postgresql} --env)"
 python -m pytest
 tests_exit_code=$?
-python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
 exit "$tests_exit_code"
