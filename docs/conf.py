ede2c416b00be34f38018f582b1e2df1f2d8e3a4
David Eckhard <dabombeeer@gmail.com>
2023-07-03 10:13:58 +0200

    docs: update intersphinx mapping due to deprecation

--- docs/conf.py
+++ docs/conf.py
@@ -327,7 +327,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    "https://docs.python.org/": None,
+    "python": ("https://docs.python.org/", None),
     "flask": ("https://flask.palletsprojects.com", None),
 }
 
